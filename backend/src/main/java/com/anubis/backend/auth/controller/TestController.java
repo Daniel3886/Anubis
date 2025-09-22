52be667b770dd24add1cbc0920696674b764e7df
Daniel <jamoja64749@gmail.com>
2025-09-22 18:20:42 +0200

    Added TestController for verifying authentication endpoints

---
+++ backend/src/main/java/com/anubis/backend/auth/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.anubis.backend.auth.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+
+    @GetMapping("/dashboard")
+    public String dashboard(@AuthenticationPrincipal OAuth2User user) {
+        if(user != null) {
+            return "Hello, OAuth2 user: " +user.getAttribute(user.getName());
+        }
+
+        return "Hello Authenticated User!";
+    }
+
+    @GetMapping("/public")
+    public String publicEndpoint(){
+        return "Public endpoint, anyone can see this";
+    }
+}
