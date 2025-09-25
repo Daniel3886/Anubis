8848190b32a95ed614a50d2734ecbeb7779450c5
Daniel <jamoja64749@gmail.com>
2025-09-25 16:50:07 +0200

    Wired endpoints to use DTOs and added forgot, change password and delete account

--- backend/src/main/java/com/anubis/backend/auth/controller/AuthController.java
+++ backend/src/main/java/com/anubis/backend/auth/controller/AuthController.java
@@ -1,8 +1,6 @@
 package com.anubis.backend.auth.controller;
 
-import com.anubis.backend.auth.dto.LoginDto;
-import com.anubis.backend.auth.dto.RegisterDto;
-import com.anubis.backend.auth.dto.VerifyEmailDto;
+import com.anubis.backend.auth.dto.*;
 import com.anubis.backend.auth.service.AuthService;
 
 import lombok.RequiredArgsConstructor;
@@ -18,31 +16,37 @@ public class AuthController {
 
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
-        String response = authService.register(
-                registerDto.getUsername(),
-                registerDto.getPassword(),
-                registerDto.getEmail()
-        );
+        String response = authService.register(registerDto);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping("/verify")
     public ResponseEntity<String> verifyEmail(@RequestBody VerifyEmailDto verifyEmailDto) {
-        String response = authService.verifyEmail(
-                verifyEmailDto.getEmail(),
-                verifyEmailDto.getCode()
-        );
+        String response = authService.verifyEmail(verifyEmailDto);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
-        String response = authService.login(
-                loginDto.getEmail(),
-                loginDto.getPassword()
-        );
+        String response = authService.login(loginDto);
         return ResponseEntity.ok(response);
     }
 
-//    TODO: Endpoints: forgot-password, change-password, delete-account
+    @PostMapping("/forgot-password")
+    public ResponseEntity<String> forgotPassword(@RequestBody ForgotPasswordDto forgotPasswordDto) {
+        String response = authService.forgotPassword(forgotPasswordDto);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/change-password")
+    public ResponseEntity<String> changePassword(@RequestBody ChangePasswordDto changePasswordDto) {
+        String response = authService.changePassword(changePasswordDto);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/delete-account")
+    public ResponseEntity<String> delete(@RequestBody DeleteAccountDto deleteAccountDto) {
+        String response = authService.deleteAccount(deleteAccountDto);
+        return ResponseEntity.ok(response);
+    }
 }
