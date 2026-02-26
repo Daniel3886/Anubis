1672d911f5f8e8938f4a2b2b9f2cc58f9a2e41e4
Daniel <jamoja64749@gmail.com>
2026-02-26 19:17:58 +0100

    refactor(User): update model to use Column, DateTimeFormat and email fields

--- backend/src/main/java/com/anubis/backend/auth/entity/User.java
+++ backend/src/main/java/com/anubis/backend/auth/entity/User.java
@@ -1,10 +1,12 @@
 package com.anubis.backend.auth.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -20,15 +22,19 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true, nullable = false)
     private String username;
 
+    @Email
+    @Column(unique = true, nullable = false)
     private String email;
 
+    @Column(unique = true)
     private String password;
 
     private boolean verified;
-
     private String verificationCode;
 
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime createdAt;
 }
